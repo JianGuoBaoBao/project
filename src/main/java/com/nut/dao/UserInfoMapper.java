c1f404ef281b1c2f904f663ceea1c175873c36f5
坚果宝宝 <791551390@qq.com>
2022-08-16 15:08:18 +0800

    pom.xml

--- src/main/java/com/nut/dao/UserInfoMapper.java
+++ src/main/java/com/nut/dao/UserInfoMapper.java
@@ -3,6 +3,8 @@ package com.nut.dao;
 import com.nut.entity.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserInfoMapper {
     int insert(UserInfo record);
@@ -10,5 +12,7 @@ public interface UserInfoMapper {
     int insertSelective(UserInfo record);
 
     int deleteByMobile(String mobile);
+
+    List<UserInfo> getUserInfoByBaseMobile(String baseMobile);
 }
 
--- src/main/java/com/nut/service/UserInfoService.java
+++ src/main/java/com/nut/service/UserInfoService.java
@@ -2,8 +2,12 @@ package com.nut.service;
 
 import com.nut.entity.UserInfo;
 
+import java.util.List;
+
 public interface UserInfoService {
-    int insert(UserInfo record);
+    void insert(UserInfo record);
+
+    List<UserInfo> getUserInfoByBaseMobile(String baseMobile);
 
     int deleteByMobile(String mobile);
 }
